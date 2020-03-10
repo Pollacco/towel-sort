16c1542dd5018ba1a041801cfa55078daebc8853
Andrey <andrey.sviontek@gmail.com>
2020-03-10 22:06:24 +0500

    solution: nice task to refresh array methods knownledge

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-    return [...matrix];  
+    let arr = [];
+    if (matrix === undefined || matrix.length === 0) return [];
+    matrix.forEach((e, index) => ((index % 2) ? arr.push(e.reverse()) : arr.push(e)));
+    return arr.flat();
 }
